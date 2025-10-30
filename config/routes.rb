29facf6395ade001929b5ab234808be3cb0ff659
Jill <jho3292@gmail.com>
2025-10-30 08:57:02 -0400

    Exclude /tasks from resources routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "tasks#index"
 
-  resources :tasks
+  resources :tasks, except: :index
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
